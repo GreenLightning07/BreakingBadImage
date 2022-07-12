dab45324d43fbf52d87ee5b5d8eabd59b940e976
GreenLightning07 <53065122+GreenLightning07@users.noreply.github.com>
2022-07-12 14:32:45 -0400

    Update ScoreBot.sh

--- ScoreBot.sh
+++ ScoreBot.sh
@@ -8,6 +8,7 @@ vsftp_found=0
 openvpn_found=0
 
 pam_configed=false
+encrypt_set=false
 
 score_report="/home/po/Desktop/ScoreReport.html"
 
@@ -61,6 +62,14 @@ update-found
 
 while true
 do
+	if ( cat /etc/pam.d/common-password | grep "pam_unix.so" | grep "remember=5" | grep "minlen=8" ); then
+		if ( cat /etc/pam.d/common-password | grep "pam_cracklib.so" | grep "ucredit=-1" | grep "lcredit=-1" | grep "dcredit=-1" | grep "ocredit=-1" ); then
+			pam_configed=true
+		fi
+	else
+		pam_configed=false
+	fi
+
 	if ( ! cat /etc/passwd | grep "kai" ); then
 		if ( cat $score_report | grep 'id="l1"' | grep "display:none" ); then
 			show-vuln "l1" "linux_found" "Linux1;" "Removed unauthorized user Kai"
@@ -93,7 +102,7 @@ do
 		hide-vuln "l4" "linux_found" "Added authorized admin Po" "Linux4;"
 	fi
 
-	if ( ! cat /etc/shadow | grep "po" | grep '$1$9GzrkEx5$QDnXTw3G.aKOHpyQxHZh.0' && $pam_configed ); then
+	if ( ! cat /etc/shadow | grep "po" | grep '$1$9GzrkEx5$QDnXTw3G.aKOHpyQxHZh.0' && $pam_configed && $encrypt_set ); then
 		if ( cat $score_report | grep 'id="l5"' | grep "display:none" ); then
 			show-vuln "l5" "linux_found" "Linux5;" "Secure password set for Po"
 		fi
@@ -141,6 +150,54 @@ do
 		hide-vuln "l10" "linux_found" "Removed malicious python script" "Linux10;"
 	fi
 
-	
+	if ( cat /etc/login.defs | grep "PASS_MAX_DAYS" | grep "90" ); then
+		if ( cat $score_report | grep 'id="l11"' | grep "display:none" ); then
+			show-vuln "l11" "linux_found" "Linux11;" "Correct max password life set"
+		fi
+	elif ( cat $score_report | grep 'id="l11"' | grep "display:block" ); then
+		hide-vuln "l11" "linux_found" "Correct max password life set" "Linux11;"
+	fi
+
+	if ( cat /etc/login.defs | grep ^"ENCRYPT_METHOD" | grep "SHA512" && cat /etc/pam.d/common-password | grep "pam_unix.so" | grep "SHA512" ); then
+		if ( cat $score_report | grep 'id="l12"' | grep "display:none" ); then
+			show-vuln "l12" "linux_found" "Linux12;" "Correct encrypt type set"
+			encrypt_set=true
+		fi
+	elif ( cat $score_report | grep 'id="l12"' | grep "display:block" ); then
+		hide-vuln "l12" "linux_found" "Correct encrypt type set" "Linux12;"
+	fi
+
+	if ( cat /etc/pam.d/common-password | grep "pam_cracklib.so" | grep "dcredit=-1" ); then
+		if ( cat $score_report | grep 'id="l13"' | grep "display:none" ); then
+			show-vuln "l13" "linux_found" "Linux13;" "Require digits in passwords"
+		fi
+	elif ( cat $score_report | grep 'id="l13"' | grep "display:block" ); then
+		hide-vuln "l13" "linux_found" "Require digits in passwords" "Linux13;"
+	fi
+
+	if ( cat /etc/apt/apt.conf.d/20auto-upgrades | grep "APT::Periodic::Download-Upgradeable-Packages \"1\";" && cat /etc/apt/apt.conf.d/20auto-upgrades | grep "APT::Periodic::Unattended-Upgrade \"1\";" ); then
+		if ( cat $score_report | grep 'id="l14"' | grep "display:none" ); then
+			show-vuln "l14" "linux_found" "Linux14;" "Automatically download and install security updates"
+		fi
+	elif ( cat $score_report | grep 'id="l14"' | grep "display:block" ); then
+		hide-vuln "l14" "linux_found" "Automatically download and install security updates" "Linux14;"
+	fi
+
+	if ( ! dpkg -l | grep "netcat"); then
+		if ( cat $score_report | grep 'id="l15"' | grep "display:none" ); then
+			show-vuln "l15" "linux_found" "Linux15;" "Netcat is removed"
+		fi
+	elif ( cat $score_report | grep 'id="l15"' | grep "display:block" ); then
+		hide-vuln "l15" "linux_found" "Netcat is removed" "Linux15;"
+	fi
+
+	if ( ! dpkg -l | grep "samba" | grep -v "python" ); then
+		if ( cat $score_report | grep 'id="l16"' | grep "display:none" ); then
+			show-vuln "l16" "linux_found" "Linux16;" "Samba is removed"
+		fi
+	elif ( cat $score_report | grep 'id="l16"' | grep "display:block" ); then
+		hide-vuln "l16" "linux_found" "Samba is removed" "Linux16;"
+	fi
+
 sleep 10
 done
