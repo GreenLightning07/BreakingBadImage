fcca42810dc0f23000ca16786ce95165eacb0d1d
GreenLightning07 <53065122+GreenLightning07@users.noreply.github.com>
2022-10-01 15:22:36 -0400

    Update scorebot.sh

--- scorebot.sh
+++ scorebot.sh
@@ -69,10 +69,10 @@ function check()
 {
 	if ( eval $1 ); then
 		if ( cat $score_report | grep "id=\"$2\"" | grep "display:none" ); then
-			show-vuln "$2" $3 "Vuln${1: -1};" "$4" "$5"
+			show-vuln "$2" $3 "Vuln${2: -1};" "$4" "$5"
 		fi
 	elif ( cat $score_report | grep "id=\"$2\"" | grep "display:block" ); then
-		hide-vuln "$2" "$3" "$4" "Vuln${1: -1};" "$5" 
+		hide-vuln "$2" "$3" "$4" "Vuln${2: -1};" "$5" 
 	fi
 }
 
