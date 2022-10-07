8bc4e935dcc733aae74371d8b5718b7b3b4b2c5f
GreenLightning07 <53065122+GreenLightning07@users.noreply.github.com>
2022-10-07 15:17:26 -0400

    Update StartScoring.sh

--- StartScoring.sh
+++ StartScoring.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 sleep 10
-git clone -b misc https://github.com/GreenLightning07/KungFuPandaImage.git
-chmod +x KungFuPandaImage/changes.sh
-chmod +x KungFuPandaImage/scorebot.sh
-mv KungFuPandaImage/scorebot.sh /var/local/scorebot.sh
-mv KungFuPandaImage/ScoreReport.html /home/po/Desktop/ScoreReport.html
-mv KungFuPandaImage/README.html /home/po/Desktop/README.html
-KungFuPandaImage/changes.sh
-sudo /var/local/scorbot.sh
+git clone -b misc https://github.com/GreenLightning07/BreakingBadImage.git
+chmod +x BreakingBadImage/changes.sh
+chmod +x BreakingBadImage/scorebot.sh
+mv BreakingBadImage/scorebot.sh /var/local/scorebot.sh
+mv BreakingBadImage/ScoreReport.html /home/walter/Desktop/ScoreReport.html
+mv BreakingBadImage/README.html /home/walter/Desktop/README.html
+BreakingBadImage/changes.sh
+sudo /var/local/scorebot.sh
