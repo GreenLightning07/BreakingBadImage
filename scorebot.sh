99d79976fbfe3290ca32e3cd188829a5ba5995f3
GreenLightning07 <53065122+GreenLightning07@users.noreply.github.com>
2022-10-09 17:06:09 -0400

    Update scorebot.sh

--- scorebot.sh
+++ scorebot.sh
@@ -12,6 +12,10 @@ function update-found
 {
 	#updates vuln found counts in score report
 	total_percent=$(awk -vn=$total_found 'BEGIN{print(n*6.667)}')
+	 echo $total_found
+        echo $total_percent
+        echo $(awk -vn=$total_found 'BEGIN{print(n*6.667)}')
+
         sed -i "s/id=\"total_found\".*/id=\"total_found\">$total_found\/15<\/h3>/g" $score_report
         sed -i "s/id=\"linux_found\".*/id=\"total_percent\">$total_percent%<\/h3>/g" $score_report
 }
